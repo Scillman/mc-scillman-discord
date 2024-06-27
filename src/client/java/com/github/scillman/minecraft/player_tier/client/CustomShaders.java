09ece08b0ca56a21f5afc38b2548a4bd702ce3f3
Scillman <Scillman@users.noreply.github.com>
2024-06-27 02:07:28 +0200

    Current state

---
+++ src/client/java/com/github/scillman/minecraft/player_tier/client/CustomShaders.java
@@ -0,0 +1,43 @@
+package com.github.scillman.minecraft.player_tier.client;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+
+import static com.github.scillman.minecraft.player_tier.PlayerTier.LOGGER;
+
+public class CustomShaders
+{
+    public static final String MOD_ID = "test";
+    public static final Identifier VERT_SHADER_ID = Identifier.of(MOD_ID, "shaders/fontrenderer.vert");
+    public static final Identifier FRAG_SHADER_ID = Identifier.of(MOD_ID, "shaders/fontrenderer.frag");
+    public static final Identifier SHADER_PROGRAM_ID = Identifier.of(MOD_ID, "shaders/fontrenderer.json");
+
+    public static void loadShaderResources()
+    {
+        ResourceManager manager = MinecraftClient.getInstance().getResourceManager();
+
+        try
+        {
+            Resource resource = manager.getResourceOrThrow(VERT_SHADER_ID);
+            InputStream stream = resource.getInputStream();
+            byte[] buffer = stream.readAllBytes();
+            String data = new String(buffer, StandardCharsets.UTF_8);
+            LOGGER.info(data);
+        }
+        catch (FileNotFoundException ex)
+        {
+
+        }
+        catch (IOException ex)
+        {
+
+        }
+    }
+}
---
+++ src/client/java/com/github/scillman/minecraft/player_tier/mixin/client/SplashOverlayMixin.java
@@ -0,0 +1,44 @@
+package com.github.scillman.minecraft.player_tier.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.struct.CallbackInjectionInfo;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.SplashOverlay;
+
+@Environment(EnvType.CLIENT)
+@Mixin(SplashOverlay.class)
+public abstract class SplashOverlayMixin
+{
+    // private void renderProgressBar(DrawContext drawContext, int minX, int minY, int maxX, int maxY, float opacity)
+    @Inject(method = "render", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/client/gui/screen/SplashOverlay;renderProgressBar(Lnet/minecraft/client/gui/DrawContext;IIIIF)V"
+    ))
+    private void onRender$1(CallbackInfo ci)
+    {
+        RenderSystem.clearColor(0.66f, 0.135f, 0.245f, 1.0f);
+        RenderSystem.clear(16384, MinecraftClient.IS_SYSTEM_MAC);
+    }
+
+    @ModifyArg(method = "render", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/DrawContext;setShaderColor(FFFF)V",
+            ordinal = 0 // first occurance
+        ),
+        index = 3 // 0 = first
+    )
+    private float onRender$2(float alpha)
+    {
+        return 0.0f;
+    }
+}
