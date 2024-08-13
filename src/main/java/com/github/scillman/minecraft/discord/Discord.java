fd0c59fe54259aeb0ec637beacb9dc41235c57cd
Scillman <Scillman@users.noreply.github.com>
2024-08-13 23:47:36 +0200

    Clean 1.21.1 repo

--- src/main/java/com/github/scillman/minecraft/discord/Discord.java
+++ src/main/java/com/github/scillman/minecraft/discord/Discord.java
@@ -1,13 +1,9 @@
 package com.github.scillman.minecraft.discord;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.scillman.minecraft.discord.network.WorldSeedPayload;
-import com.github.scillman.minecraft.discord.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
 
 public class Discord implements ModInitializer
 {
@@ -19,8 +15,5 @@ public class Discord implements ModInitializer
     @Override
     public void onInitialize()
     {
-        PayloadTypeRegistry.playS2C().register(WorldSeedPayload.ID, WorldSeedPayload.CODEC);
-
-        ModItems.register();
     }
 }
